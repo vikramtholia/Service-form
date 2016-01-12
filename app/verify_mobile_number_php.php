d3526e3bb0b357a8f7804785d8928db22ac88243
vikramtholia <vikramtholia2@gmail.com>
2016-01-12 20:57:17 +0530

    Create verify_mobile_number_php.php

---
+++ app/verify_mobile_number_php.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['verification_code'])){
+ if (empty($_POST['verification_code'])){ 
+ // Setting error message
+ $_SESSION['error_verify_mobile_number'] = "Verification code is missing, Please fill it again";
+ header("location: verify_moblie_number_form.php"); // Redirecting to first page 
+ } else {
+ if (!preg_match("/^[0-9]{6}$/", $_POST['verification_code'])){ 
+ $_SESSION['error_verify_mobile_number'] = "Please enter 6 digit verification code.";
+ header("location: verify_moblie_number_form.php");
+ } else {
+ if (($_SESSION['sent_verification_code']) === ($_POST['verification_code'])) {
+  //$_SESSION['post'][$key] = $value;
+ header("location: page2_form.php");
+ }
+ } else {
+ $_SESSION['error_verify_mobile_number'] = "Verification code does not match. Please try again";
+ header("location: verify_moblie_number_form.php"); //redirecting to verification page
+ }
+ }
+ } 
+header("location: verify_moblie_number_form.php"); //If everything is fine then transfer for mobile number verification
+?>
