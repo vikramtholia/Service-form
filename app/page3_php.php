fa36eb22f995e2e9574f9251ee7b94dac2f2f4af
vikramtholia <vikramtholia2@gmail.com>
2016-01-13 00:59:51 +0530

    Update page3_php.php

--- app/page3_php.php
+++ app/page3_php.php
@@ -2,13 +2,10 @@
 session_start();
 // Checking second page values for empty, If it finds any blank field then redirected to second page.
  if (isset($_POST['address1'])){
- if (empty($_POST['make'])
- || empty($_POST['gadget_type'])
- || empty($_POST['model'])
- || empty($_POST['problem_type'])
- || empty($_POST['problem-description'])){ 
- $_SESSION['error_page2'] = "Mandatory field(s) are missing, Please fill it again"; // Setting error message.
- header("location: page2_form.php"); // Redirecting to second page. 
+ if (empty($_POST['address1'])
+ || empty($_POST['area'])){ 
+ $_SESSION['error_page3'] = "Mandatory field(s) are missing, Please fill it again"; // Setting error message.
+ header("location: page3_form.php"); // Redirecting to third page. 
  } else {
  // Fetching all values posted from second page and storing it in variable.
  foreach ($_POST as $key => $value) {
@@ -20,4 +17,4 @@ session_start();
  header("location: page1_form.php");// Redirecting to first page. */
  }
 } 
-header("location: page3_form.php");
+header("location: page4_form.php");
