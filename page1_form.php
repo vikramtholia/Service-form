e5c11eebac1bda1e436327a31a36c20945ef6b90
vikramtholia <vikramtholia2@gmail.com>
2016-01-14 18:43:26 +0530

    Update page1_form.php

--- page1_form.php
+++ page1_form.php
@@ -22,12 +22,12 @@ session_start(); // Session starts here.
  </span>
  <form action="app/page1_php.php" method="post">
  <label>Full Name :<span>*</span></label>
- <input name="name" type="text" placeholder="Ex-Sachin Sharma" required>
+ <input name="name" type="text" placeholder="Ex-Sachin Sharma" required><?php if(isset($_SESSION['post']['name']) echo $_SESSION['post']['name']; ?></input>
  <label>Mobile :<span>*</span></label>
- <input name="contact" type="text" placeholder="10-digit number" required>
+ <input name="contact" type="text" placeholder="10-digit number" required><?php if(isset($_SESSION['post']['contact']) echo $_SESSION['post']['contact']; ?></input>
  <p>We will verify your mobile number by sending verification code by sms.</p>
  <label>Email :<span>*</span></label>
- <input name="email" type="email" placeholder="Ex-sachin@gmail.com" required>
+ <input name="email" type="email" placeholder="Ex-sachin@gmail.com" required><?php if(isset($_SESSION['post']['email']) echo $_SESSION['post']['email']; ?></input>
  <p>Important: We will send service status and details on this email address.</p>
  <label>Password :<span>*</span></label>
  <input name="password" type="Password" placeholder="********" />
