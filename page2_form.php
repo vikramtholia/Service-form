e126d8771dd0ff019533b7a5c1285dac75853b6c
vikramtholia <vikramtholia2@gmail.com>
2016-01-15 01:27:37 +0530

    Update page2_form.php

--- page2_form.php
+++ page2_form.php
@@ -1,50 +1,11 @@
 <?php
 session_start();
-// Checking first page values for empty,If it finds any blank field then redirected to first page.
-/* if (isset($_POST['name'])){
- if (empty($_POST['name'])
- || empty($_POST['email'])
- || empty($_POST['contact'])
- || empty($_POST['password'])
- || empty($_POST['confirm'])){ 
- // Setting error message
- $_SESSION['error'] = "Mandatory field(s) are missing, Please fill it again";
- header("location: page1_form.php"); // Redirecting to first page 
- } else {
- // Sanitizing email field to remove unwanted characters.
- $_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); 
- // After sanitization Validation is performed.
- if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ 
- // Validating Contact Field using regex.
- if (!preg_match("/^[0-9]{10}$/", $_POST['contact'])){ 
- $_SESSION['error'] = "10 digit contact number is required.";
- header("location: page1_form.php");
- } else {
- if (($_POST['password']) === ($_POST['confirm'])) {
- foreach ($_POST as $key => $value) {
- $_SESSION['post'][$key] = $value;
- }
- } else {
- $_SESSION['error'] = "Password does not match with Confirm Password.";
- header("location: page1_form.php"); //redirecting to first page
- }
- }
- } else {
- $_SESSION['error'] = "Invalid Email Address";
- header("location: page1_form.php");//redirecting to first page
- }
- }
-} else {
- if (empty($_SESSION['error_page2'])) {
- header("location: page1_form.php");//redirecting to first page
- }
-} */
 ?> 
 <!DOCTYPE HTML>
 <html>
  <head>
  <title>Gadget Details</title>
- <link rel="stylesheet" href="style.css" />
+ <link rel="stylesheet" href="app/style.css" />
  </head>
  <body>
  <div class="container">
