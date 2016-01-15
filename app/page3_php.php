80e376f13614459c020053e18f2f99ff193e2d22
vikramtholia <vikramtholia2@gmail.com>
2016-01-15 21:25:18 +0530

    Update page3_php.php

--- app/page3_php.php
+++ app/page3_php.php
@@ -1,20 +1,43 @@
 <?php
 session_start();
-// Checking second page values for empty, If it finds any blank field then redirected to second page.
- if (isset($_POST['address1'])){
- if (empty($_POST['address1'])
- || empty($_POST['area'])){ 
- $_SESSION['error_page3'] = "Mandatory field(s) are missing, Please fill it again"; // Setting error message.
- header("location: page3_form.php"); // Redirecting to third page. 
- } else {
- // Fetching all values posted from second page and storing it in variable.
- foreach ($_POST as $key => $value) {
- $_SESSION['post'][$key] = $value;
- }
- }
-} else {
- /* if (empty($_SESSION['error_page3'])) {
- header("location: page1_form.php");// Redirecting to first page. */
- }
-} 
-header("location: page4_form.php");
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+$areaErr = $address1Err =  "";
+$area = $address1 = $address2 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["area"])) {
+     $areaErr = "Please select your area. If your area is not listed then please select last option <strong>My area is not listed</strong></br>";
+   } else {
+     $area = test_input($_POST["area"]);
+   }
+   
+   if (empty($_POST["address1"])) {
+     $address1Err = "Please fill your address in <strong>Address1</strong>field.</br>";
+   } else {
+     $address1 = test_input($_POST["address1"]);
+   }
+   
+   $address2 = test_input($_POST["address2"]);
+   
+   if($areaErr != "" || $address1Err != "" ) {
+    $_SESSION['error_page2'] = $areaErr + $address1Err;
+    header("location: page3_form.php");
+   }
+   
+   $_SESSION['post']['area'] = $area;
+   $_SESSION['post']['address1'] = $address1;
+   $_SESSION['post']['address2'] = $address2;
+   
+   header("location: page4_form.php");
+   
+}
+else {
+ echo "The operation you are trying to do can't be complete.";
+ header("location: badoperation.php");
+}
