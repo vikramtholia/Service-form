8dd0e62e7a16e23e732aea77ad90cf5109d5a2c8
vikramtholia <vikramtholia2@gmail.com>
2016-01-28 18:27:31 +0530

    Update sendsms.php

--- sendsms.php
+++ sendsms.php
@@ -1,1 +1,22 @@
-
+if(!empty($smsmessage) && !empty($smscontact))
+{
+  
+  $smsusername = ;
+  $smspassword = ;
+  $smssenderid = ;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'url');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $data = curl_exec($ch);
+  if($data === "success")
+  {
+    echo "message sent";
+  }
+  else 
+  {
+    echo $data;
+  }
+}
+else {
+  echo "mobile number or message is empty";
+}
