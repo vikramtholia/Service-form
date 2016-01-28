0476c6c79850881f23c9ddf68ab7d02512cb2053
vikramtholia <vikramtholia2@gmail.com>
2016-01-28 16:27:06 +0530

    Update changeorderstatus.php

--- changeorderstatus.php
+++ changeorderstatus.php
@@ -1,2 +1,31 @@
 <?php
+session_start();
 
+if(!empty($_SESSION['luserid']) && !empty($_SESSION['luserid']))
+{
+  $orderId= $_REQUEST['orderid'];
+  if(empty($orderId))
+  {
+    die "order is empty on page changeorderstatus";
+  }
+  
+  include '../include/dbconn.php'
+  $sql = "SELECT * from order";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } 
+  else {
+    echo "0 results";
+  }
+
+  mysqli_close($conn);
+  
+}
+else {
+  echo "You are not authorized to view this page!!";
+}
